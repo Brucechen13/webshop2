93a6e627ebb64a4ab48b6503d2dd0be04756a64b
Brucechen13 <chenchi_94@163.com>
2017-05-03 19:07:21 +0800

    build freeworker and spring

--- src/main/java/com/cc/webstore/MainServlet.java
+++ src/main/java/com/cc/webstore/MainServlet.java
@@ -1,5 +1,11 @@
 package com.cc.webstore;
 
+import com.cc.webstore.beans.Product;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,8 +19,28 @@ import java.io.IOException;
 
 @WebServlet("/hello")
 public class MainServlet extends HttpServlet{
+    private Configuration cfg;
+    @Override
+    public void init() throws ServletException {
+        //initialize operation
+        cfg=new Configuration(Configuration.VERSION_2_3_25);
+        cfg.setServletContextForTemplateLoading(getServletContext(), "WEB-INF/templates");
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+    }
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        //super.doGet(req, resp);
+        Product root = new Product();
+        //得到模板
+        try {
+            //将模板和数据结合，并返回浏览器
+            Template template=cfg.getTemplate("test.ftl");
+            template.process(root, resp.getWriter());
+        } catch (Exception e) {
+            //e.printStackTrace();
+            resp.getWriter().print("暂时无数据");
+        }
     }
 }
---
+++ src/main/java/com/cc/webstore/beans/Product.java
@@ -0,0 +1,26 @@
+package com.cc.webstore.beans;
+
+/**
+ * Created by chenc on 2017/5/3.
+ */
+public class Product {
+
+    private String url;
+    private String productName;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+}
